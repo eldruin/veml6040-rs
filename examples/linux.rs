ca74a130354f9ce95d9d013b2bb2c21a5d7d02a7
Diego Barrios Romero <eldruin@gmail.com>
2018-10-22 21:13:44 +0200

    Add linux example

---
+++ examples/linux.rs
@@ -0,0 +1,19 @@
+extern crate linux_embedded_hal;
+extern crate veml6040;
+
+use linux_embedded_hal::I2cdev;
+use veml6040::Veml6040;
+
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut sensor = Veml6040::new(dev);
+    sensor.enable().unwrap();
+    
+    let red = sensor.read_red_channel().unwrap();
+    let green = sensor.read_green_channel().unwrap();
+    let blue = sensor.read_blue_channel().unwrap();
+    let white = sensor.read_white_channel().unwrap();
+
+    println!("Measurements: red = {}, green = {}, blue = {}, white = {}",
+             red, green, blue, white);
+}
